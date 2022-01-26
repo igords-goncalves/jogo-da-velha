df570778d9013419c57d7315db866ff9cbb03850
Igor Gonçalves <igordossantos38@gmail.com>
2022-01-26 18:53:59 -0300

    Tentando criar a lógica, e enfrentando um problema em relação appendChild

--- source/js/index.js
+++ source/js/index.js
@@ -1,5 +1,37 @@
-const botao = document.querySelector("#vezes")
+const x = document.querySelector(".x")
+const o = document.querySelector(".o")
 
-botao.addEventListener('click', () => {
-    console.log('clicado!')
-})
+const boxes = document.querySelectorAll(".box")
+const botoes = document.querySelector("#botoes button")
+
+const mensagem = document.querySelector("#mensagem")
+const textoMenssagem = document.querySelector("#mensagem p")
+
+// Contador de jogadas
+
+let player1 = 0
+let player2 = 0
+
+// Adcionando o evento de click quando clicar no quadro
+
+for (let i = 0; i < boxes.length; i++) {
+
+    boxes[i].addEventListener('click', () => {
+
+        let elemento
+
+        if (player1 == player2) {
+            elemento = x
+        } else {
+            elemento = o
+        }
+
+        let cloneElemento = elemento.cloneNode(true)
+
+        console.log(cloneElemento) 
+
+        //? Como fazer ele aparecer em acada elemento do evento, que são as boxes
+
+        //...
+    })
+}
