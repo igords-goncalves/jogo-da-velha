c695fde417a5e8d98b5a4009201e3efca229a6b6
Igor Gonçalves <igordossantos38@gmail.com>
2022-01-26 17:12:17 -0300

    CSS pronto

---
+++ README.md
@@ -0,0 +1,6 @@
+# Jogo da Velha
+
+- Projeto baseado no modelo da pagina inicial do google
+
+- O x sempre começa primeiro
+
---
+++ source/js/index.js
@@ -0,0 +1,5 @@
+const botao = document.querySelector("#vezes")
+
+botao.addEventListener('click', () => {
+    console.log('clicado!')
+})
