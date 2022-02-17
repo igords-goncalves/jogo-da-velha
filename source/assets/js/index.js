9e569a7e69d10f139eaed71304b99508ec056cc5
Igor Gonçalves <igordossantos38@gmail.com>
2022-02-17 12:11:51 -0300

    Toda lógica de vitória está pronta e a exibição da mensagem de vitória também, a própria mensagem bloqueia a continuação do game

--- source/assets/js/index.js
+++ source/assets/js/index.js
@@ -3,7 +3,7 @@ const o = document.querySelector(".o")
 const boxes = document.querySelectorAll('.box') // Nodelist estática []
 const botoes = document.querySelectorAll("#botoes")
 const mensagem = document.querySelector("#mensagem")
-const textoMenssagem = document.querySelector("#mensagem p")
+let textoMensagem = document.querySelector("#mensagem p")
 
 // Contador de jogadas
 
@@ -51,24 +51,20 @@ function checarElemento(player1, player2) {
     return elemento
 }
 
-//%* Logica da condicão de vitória
+//%* Mapeamento da condicão de vitória
 
 function checarCondicaoDeVitoria() {
 
     let b1 = document.querySelector('#block-1')
     let b2 = document.querySelector('#block-2')
     let b3 = document.querySelector('#block-3')
+    let b4 = document.querySelector('#block-4')
+    let b5 = document.querySelector('#block-5')
+    let b6 = document.querySelector('#block-6')
+    let b7 = document.querySelector('#block-7')
+    let b8 = document.querySelector('#block-8')
+    let b9 = document.querySelector('#block-9')
 
-    // Horizontal linha 1 2 3
-
-        // if (b1.childNodes.length > 0 && b2.childNodes.length > 0 && b3.childNodes.length > 0) {
-        //     if (
-        //         b1.childNodes[0].className === 'x' 
-        //         && b2.childNodes[0].className === 'x'
-        //         && b3.childNodes[0].className === 'x') {
-        //         console.log('x venceu')
-        //     }
-        // }
 
         function condicaoDeVitoria(node1, node2, node3, type) {
             if(
@@ -81,10 +77,84 @@ function checarCondicaoDeVitoria() {
                     node2.childNodes[0].className === type &&
                     node3.childNodes[0].className === type
                     ) {
-                    console.log(`${type} venceu`)
+                    declararVencedor(type)
                 }
             }
         }
+
+
+    let empate = 0;
+    for (let index = 0; index < boxes.length; index++) {
+        if (boxes[index].childNodes[0] != undefined) {
+            empate ++
+        }        
+    }
+    if(empate === 9) {
+        declararVencedor()
+    }
+
+    // Horizontal linha
+
     condicaoDeVitoria(b1, b2, b3, 'x')
     condicaoDeVitoria(b1, b2, b3, 'o')
+
+    condicaoDeVitoria(b4, b5, b6, 'x')
+    condicaoDeVitoria(b4, b5, b6, 'o')
+
+    condicaoDeVitoria(b7, b8, b9, 'x')
+    condicaoDeVitoria(b7, b8, b9, 'o')
+
+    // Vertical linha
+
+    condicaoDeVitoria(b1, b4, b7, 'x')
+    condicaoDeVitoria(b1, b4, b7, 'o')
+
+    condicaoDeVitoria(b2, b5, b8, 'x')
+    condicaoDeVitoria(b2, b5, b8, 'o')
+
+    condicaoDeVitoria(b3, b6, b9, 'x')
+    condicaoDeVitoria(b3, b6, b9, 'o')
+
+    // Diagonal linha
+
+    condicaoDeVitoria(b1, b5, b9, 'x')
+    condicaoDeVitoria(b1, b5, b9, 'o')
+
+    condicaoDeVitoria(b3, b5, b7, 'x')
+    condicaoDeVitoria(b3, b5, b7, 'o')
+}
+
+// Limpa o jogo, declara o vencedor e atualiza o placar
+
+function declararVencedor(vencedor) {
+    let placarX = document.querySelector('span #score-1')
+    let placarO = document.querySelector('span #score-2')
+
+    let msg = '';
+
+    if (vencedor === 'x') {
+        placarX.textContent = parseInt(placarX.textContent) + 1
+        msg = "X VENCEU"
+        trocarBg()
+    } else if(vencedor === 'o') {
+        placarO.textContent = parseInt(placarO.textContent) + 1
+        msg = "O VENCEU"
+        trocarBg()
+    } else {
+        msg = 'EMPATE'
+        trocarBg()
+    }
+
+    // Exibindo mensagem 
+    textoMensagem.innerHTML = msg
+    mensagem.classList.remove('esconder')
+
+    function trocarBg() {
+        const bgQuadro = document.querySelector("#bg-quadro")
+        const quadro = document.querySelector("#quadro")
+        bgQuadro.style.filter = 'blur(8px)'
+        quadro.style.filer = 'blur(5px)'
+        bgQuadro.style.transition = '0.3s'
+        quadro.style.transition = '0.2s'
+    }
 }
