41eaef56e5b0286ae4c05375fc8183524570d5f5
Igor Gonçalves <igordossantos38@gmail.com>
2024-12-24 12:16:59 -0300

    refactor: simplify game initialization by removing unused variables and parameters

--- source/assets/js/index.js
+++ source/assets/js/index.js
@@ -1,11 +1,5 @@
 import clicarEinserirSimboloTratado from "./utils/clicarEinserirSimboloTratado.js";
 import recomecarPartidaDoZero from "./utils/recomecarPartidaDoZero.js";
-const containerDeCaixas = document.querySelectorAll(".box");
-
-const x = document.querySelector(".x");
-const o = document.querySelector(".o");
-let player = 0;
-let ia = 0;
 
 recomecarPartidaDoZero();
-clicarEinserirSimboloTratado(containerDeCaixas, x, o, player, ia);
+clicarEinserirSimboloTratado();
