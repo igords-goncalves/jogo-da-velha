c8d6291d8336ecd34255c1ff5488fad70740f18b
Igor Gonçalves <igordossantos38@gmail.com>
2024-12-24 16:56:39 -0300

    refactor: remove unused functions related to game logic

--- source/assets/js/functions/checarValorEdefinirSimbolo.js
+++
@@ -1,8 +0,0 @@
-function checarValorEdefinirSimbolo(x, o, player, ia) {
-  let simboloDaVariavel = "";
-
-  player === ia ? (simboloDaVariavel = x) : (simboloDaVariavel = o);
-  return simboloDaVariavel;
-}
-
-export default checarValorEdefinirSimbolo;
